8e24ca71603ce6e8f6ab4a4591c677d78d803e1f
Chanhyuck Ko <limeelbee@gmail.com>
2024-08-02 17:20:30 +0900

    temp

--- NineChronicles.Headless.Executable/Commands/DPoSGenesisCommand.cs
+++ NineChronicles.Headless.Executable/Commands/DPoSGenesisCommand.cs
@@ -30,7 +30,7 @@ public class DPoSGenesisCommand
         [Argument("CONFIG", Description = "JSON config path to mine genesis block")]
         string configPath)
     {
-        /*var options = new JsonSerializerOptions
+        var options = new JsonSerializerOptions
         {
             AllowTrailingCommas = true,
             PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
@@ -43,15 +43,16 @@ public class DPoSGenesisCommand
         string stateStorePath = Path.Combine(storePath, "states");
         IStateStore stateStore = new TrieStateStore(new RocksDBKeyValueStore(stateStorePath));
         var block = DPoSBlockHelper.ProposeGenesisBlock(
-            privateKey,
-            stateStore,
-            initialNCGs.ToDictionary(
+            stateStore: stateStore,
+            privateKey: privateKey,
+            goldCurrency: null,
+            initialNCGs: initialNCGs.ToDictionary(
                 v => new Address(v.Address),
                 v => (BigInteger)v.Amount),
-            validators.ToDictionary(
+            initialValidators: validators.ToDictionary(
                 v => PublicKey.FromHex(v.PublicKey),
                 v => (BigInteger)v.Power));
-        Lib9cUtils.ExportBlock(block, "genesis-block");*/
+        Lib9cUtils.ExportBlock(block, "genesis-block");
     }
     
 #pragma warning disable S3459
